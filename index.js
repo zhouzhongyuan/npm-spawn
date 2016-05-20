7d40e5f1f236d17c13b5545b2d8cda8aca560622
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-05-20 10:30:02 +0800

    fix process.env;

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const spawn = require('child_process').spawn;
+const _ = require('underscore');
 var npmCmd = function( args, options){
+    console.log(`npm ${args} begin`);
     return new Promise(function(resolve,reject){
+        if(options && options.env){
+            var env = Object.create( process.env );
+            env = _.extend(env, options.env);
+            options.env = env;
+        }
         var command = options.command;
         if (!command) {
             command = process.platform === 'win32'? 'npm.cmd': 'npm';
         }
+        console.log(command, args, options);
         var cmd = spawn(command, args, options);
         cmd.stdout.on('data', (data) => {
             console.log(`stdout: ${data}`);
         });
         cmd.stderr.on('data', (data) => {
             console.log(`stderr: ${data}`);
-            reject(`stderr: ${data}`);
+            //reject(`stderr: ${data}`);
         });
-        cmd.on('close', (code) => {
-            console.log(`child process exited with code ${command} ${args} ${code}`);
-            resolve(`${command} ${args} ${code}`);
+        cmd.on('close', (code,signal) => {
+            console.log(`CLOSE ${signal}'s code is ${code}`);
+            resolve(`CLOSE ${signal}'s code is ${code}`);
+        });
+        cmd.on('error', (error) => {
+            console.log('REEOR');
+            console.log(error);
+        });
+        cmd.on('exit', (code,signal) => {
+            console.log(`EXIT ${signal}'s code is ${code}`);
+        });
+        cmd.on('message', (message) => {
+            console.log('MESSAGE');
+            console.log(message);
         });
     })
 }
